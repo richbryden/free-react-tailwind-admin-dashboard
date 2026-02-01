6edc073fca7077fd3360af11e9930f3d6d4b54ae
richbryden <richbryden@gmail.com>
2026-02-01 22:08:48 +0000

    Revising Superstore.tsx to use Tableau API v3 method

--- src/pages/Tableau/Superstore.tsx
+++ src/pages/Tableau/Superstore.tsx
@@ -1,56 +1,44 @@
 import { useEffect, useRef } from 'react';
 
+// Declaration to silence TypeScript errors regarding the custom element
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'tableau-viz': any;
+    }
+  }
+}
+
 const Superstore = () => {
-  const vizRef = useRef<HTMLDivElement>(null);
+  const vizRef = useRef<any>(null);
 
   useEffect(() => {
-    const divElement = vizRef.current;
-    if (!divElement) return;
-
-    const url = "https://public.tableau.com/javascripts/api/viz_v1.js";
-    const existingScript = document.querySelector(`script[src="${url}"]`);
+    // 1. Load the Modern Tableau v3 Script
+    const scriptUrl = "https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js";
+    const existingScript = document.querySelector(`script[src="${scriptUrl}"]`);
 
     if (!existingScript) {
       const scriptElement = document.createElement('script');
-      scriptElement.src = url;
+      scriptElement.src = scriptUrl;
+      scriptElement.type = 'module'; // v3 uses ES modules
       scriptElement.async = true;
-      divElement.parentNode?.insertBefore(scriptElement, divElement);
+      document.body.appendChild(scriptElement);
     }
   }, []);
 
   return (
-    <div className="h-screen w-full p-4 bg-white rounded-sm border border-stroke shadow-default dark:border-strokedark dark:bg-boxdark">
-      <div 
-        className='tableauPlaceholder' 
-        id='viz1769981011138' 
-        style={{ position: 'relative', width: '100%', height: '100%', overflow: 'hidden' }} 
+    <div className="w-full h-[85vh] p-4 bg-white rounded-sm border border-stroke shadow-default dark:border-strokedark dark:bg-boxdark">
+      {/* 2. Use the Modern Web Component */}
+      <tableau-viz 
         ref={vizRef}
+        id="tableauViz"
+        src="https://public.tableau.com/views/SuperstoreDashboard_16709573699130/SuperstoreDashboard"
+        toolbar="bottom"
+        hide-tabs
+        height="100%"
+        width="100%"
       >
-        <noscript>
-          <a href='#'>
-            <img 
-              alt='Superstore Dashboard' 
-              src='https://public.tableau.com/static/images/Su/SuperstoreDashboard_16709573699130/SuperstoreDashboard/1_rss.png' 
-              style={{ border: 'none' }} 
-            />
-          </a>
-        </noscript>
-        <object className='tableauViz' style={{ display: 'none' }}>
-          <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-          <param name='embed_code_version' value='3' />
-          <param name='site_root' value='' />
-          <param name='name' value='SuperstoreDashboard_16709573699130/SuperstoreDashboard' />
-          <param name='tabs' value='no' />
-          <param name='toolbar' value='yes' />
-          <param name='static_image' value='https://public.tableau.com/static/images/Su/SuperstoreDashboard_16709573699130/SuperstoreDashboard/1.png' />
-          <param name='animate_transition' value='yes' />
-          <param name='display_static_image' value='yes' />
-          <param name='display_spinner' value='yes' />
-          <param name='display_overlay' value='yes' />
-          <param name='display_count' value='yes' />
-          <param name='language' value='en-US' />
-        </object>
-      </div>
+      </tableau-viz>
     </div>
   );
 };
